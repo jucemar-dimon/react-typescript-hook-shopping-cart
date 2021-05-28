1c99729445f299c9d0ec07ca006aaa01e89cd7b9
Jucemar Dimon <jucemar.dimon@gmail.com>
2021-05-28 18:44:45 -0300

    Conclui o desafio principal do capítulo II da Trilha ReactJS do curso Ignite

---
+++ README.md
@@ -0,0 +1,13 @@
+# :rocket: Rocketseat - Ignite - ReactJS
+## Capítulo II - Desafio principal
+Este desafio teve como objetivo principal a criação de um hook de carrinho de compras.
+### Conceitos envolvidos
+- [x] CSS-in-JS
+- [x] Hooks
+- [x] Context API
+
+### Bibliotecas
+- [x] json-server
+- [x] react-toastify
+- [x] styled-components
+- [x] axios
--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -23,28 +23,64 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
-
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
-
+    const storagedCart = localStorage.getItem('@RocketShoes:cart');
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
-    } catch {
-      // TODO
+      const productStock = await api.get(`/stock/${productId}`).then(response => response.data);
+      const product = await api.get(`/products/${productId}`).then(response => response.data);
+      const productInCart = await cart.find(item => item.id === productId);
+      
+      if(!product){
+        return;
+      }
+      if (productStock.amount > 0) {
+        if (productInCart) {
+          if (productInCart.amount + 1 > productStock.amount) {
+            toast.error('Quantidade solicitada fora de estoque');
+          } else {
+            const newCartList = cart.map(item => {
+              if (item.id === productInCart.id) {
+                return { ...item, amount: item.amount + 1 };
+              }
+              return item;
+            });
+            setCart(newCartList);
+            localStorage.setItem('@RocketShoes:cart', JSON.stringify(newCartList));
+          }
+        } else {
+          setCart([...cart, { ...{ ...product }, amount: 1 }])
+          localStorage.setItem('@RocketShoes:cart', JSON.stringify([...cart, { ...product, amount: 1 }]));
+
+        }
+      } else {
+        toast.error('Quantidade solicitada fora de estoque');
+      }
+
+    } catch (error) {
+      console.log(error);
+      toast.error('Erro na adição do produto');
     }
+
   };
 
-  const removeProduct = (productId: number) => {
+  const removeProduct = async (productId: number) => {
     try {
-      // TODO
+      const product = cart.find(itemCart=>itemCart.id=== productId);
+      if (!product) {
+        throw new Error(`Product doesn't existis`);
+      } else {
+        const filteredCard = cart.filter(product => product.id !== productId);
+        setCart(filteredCard);
+        localStorage.setItem('@RocketShoes:cart', JSON.stringify(filteredCard));
+      }
     } catch {
-      // TODO
+      toast.error('Erro na remoção do produto');
     }
   };
 
@@ -53,9 +89,32 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
-      // TODO
+     
+        const productStock = await api.get(`/stock/${productId}`).then(response => response.data);
+        const productInCart=cart.find(itemCart=>itemCart.id===productId);
+
+        if(amount<1){
+          return;
+
+        }
+
+        if (productStock.amount < amount ) {
+          toast.error('Quantidade solicitada fora de estoque');
+        } else {
+          const cartUpdated = cart.map(product => {
+            if (product.id === productId) {
+              return { ...product, amount: amount }
+            }
+            return product;
+          })
+
+          setCart(cartUpdated);
+
+          localStorage.setItem('@RocketShoes:cart', JSON.stringify(cartUpdated));
+        }
+      
     } catch {
-      // TODO
+      toast.error('Erro na alteração de quantidade do produto');
     }
   };
 
